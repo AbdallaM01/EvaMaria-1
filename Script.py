1886d843c140e760a3fe42fe9323faa2b801adca
AbdallaM01 <100961487+AbdallaM01@users.noreply.github.com>
2022-09-14 22:23:38 +0200

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,28 +1,36 @@
 class script(object):
     START_TXT = """𝙷𝙴𝙻𝙾 {},
-𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href=https://t.me/{}>{}</a>, 𝙸 𝙲𝙰𝙽 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝙼𝙾𝚅𝙸𝙴𝚂, 𝙹𝚄𝚂𝚃 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 𝙰𝙽𝙳 𝙴𝙽𝙹𝙾𝚈 😍"""
+𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href=https://t.me/{}>{}</a>, 𝙸 𝙲𝙰𝙽 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝚂𝚎𝚛𝚒𝚎𝚜, 𝙹𝚄𝚂𝚃 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 𝙰𝙽𝙳 𝙴𝙽𝙹𝙾𝚈 😍"""
     HELP_TXT = """𝙷𝙴𝚈 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
     ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
-✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: ArrowFlix
+✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/ArrowFlix>𝙰𝚛𝚛𝚘𝚠𝙵𝚕𝚒𝚡</a> 
 ✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
 ✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
 ✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
 ✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
-✯ 𝙲𝚁𝙴𝙰𝙳𝙸𝚃  : 𝚃𝚎𝚊𝚖 𝙴𝚟𝚊 𝙼𝚊𝚛𝚒𝚊
 ✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
     SOURCE_TXT = """<b>NOTE:</b>
-- 𝑺𝒏𝒐𝒘 𝑾𝒉𝒊𝒕𝒆 is a open source project. 
-- Source - https://t.me/danger_bots  
+𝚂𝚎𝚛𝚒𝚎𝚜 𝚌𝚑𝚊𝚗𝚗𝚎𝚕
+https://t.me/+zqd9DiraQkM2NWZk
+
+𝙼𝚘𝚟𝚒𝚎𝚜 𝚌𝚑𝚊𝚗𝚗𝚎𝚕 
+https://t.me/ArrowFlixMovies
+
+𝙼𝚊𝚒𝚗 𝚌𝚑𝚊𝚗𝚗𝚎𝚕
+https://t.me/TorrentSerie
+
+𝙶𝚛𝚘𝚞𝚙
+https://t.me/ArrowFlix
 
 <b>DEVS:</b>
-- <a href=https://t.me/danger_bots>Team dangerbots</a>"""
+- <a href=https://t.me/ArrowFlix>𝙰𝚛𝚛𝚘𝚠𝙵𝚕𝚒𝚡>Team dangerbots</a>"""
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 
 - Filter is the feature were users can set automated replies for a particular keyword and 𝑺𝒏𝒐𝒘 𝑾𝒉𝒊𝒕𝒆 will respond whenever a keyword is found the message
 
 <b>NOTE:</b>
-1. 𝑺𝒏𝒐𝒘 𝑾𝒉𝒊𝒕𝒆 should have admin privillage.
+1. 𝙰𝚛𝚛𝚘𝚠𝙵𝚕𝚒𝚡 should have admin privillage.
 2. only admins can add filters in a chat.
 3. alert buttons have a limit of 64 characters.
 
@@ -37,11 +45,11 @@ class script(object):
 
 <b>NOTE:</b>
 1. Telegram will not allows you to send buttons without any content, so content is mandatory.
-2. 𝑺𝒏𝒐𝒘 𝑾𝒉𝒊𝒕𝒆 supports buttons with any telegram media type.
+2. 𝙰𝚛𝚛𝚘𝚠𝙵𝚕𝚒𝚡 supports buttons with any telegram media type.
 3. Buttons should be properly parsed as markdown format
 
 <b>URL buttons:</b>
-<code>[Button Text](buttonurl:https://t.me/db_autofilter_bot)</code>
+<code>[Button Text](buttonurl:https://t.me/ArrowFlixbot)</code>
 
 <b>Alert buttons:</b>
 <code>[Button Text](buttonalert:This is an alert message)</code>"""
