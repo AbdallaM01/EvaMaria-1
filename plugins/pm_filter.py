0596c0a04307a0b0f3e1ef9999a4b4eae6a2de5b
AbdallaM01 <100961487+AbdallaM01@users.noreply.github.com>
2022-09-14 22:54:44 +0200

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -439,7 +439,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
     elif query.data == "about":
         buttons = [[
             InlineKeyboardButton('🤖 Updates', url='https://t.me/ArrowFlix'),
-            InlineKeyboardButton('⚡ Channels', callback_data='source')
+            InlineKeyboardButton('⚡ Series', url='https://t.me/+zqd9DiraQkM2NWZk')
         ], [
             InlineKeyboardButton('🏠 Home', callback_data='start'),
             InlineKeyboardButton('🔐 Close', callback_data='close_data')
